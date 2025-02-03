757efbc9752aa72adfab3f7d736f29972a26970d
Ricardo Diaz <kerackdarcku@gmail.com>
2025-02-03 13:24:20 -0500

    migrations update

--- Tehu-back/migrations/20240508023814-neew-fields-remove-old.js
+++ Tehu-back/migrations/20240508023814-neew-fields-remove-old.js
@@ -3,14 +3,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('devices', 'destination', {
-      type: Sequelize.STRING
-    });
+    return Promise.resolve();
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('devices', 'destination');
     await queryInterface.removeColumn('devices', 'lgn_start');
     await queryInterface.removeColumn('devices', 'lgn_end');
-  }
+  }
 };
